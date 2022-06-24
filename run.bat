c7ae828ad89c3e12afc5499225e9755917b0fb4e
Daniel Stirling <dannystirl@gmail.com>
2022-06-24 19:50:45 -0400

    maven repo is set up now

--- run.bat
+++ run.bat
@@ -5,4 +5,10 @@ javac -d bin/ -cp "." src/main/java/destiny/*.java
 rd /q /s "output" 2>nul
 mkdir output
 
-java -cp bin WishlistGenerator %1 > output\WishListScripted.txt
+:: java -cp bin WishlistGenerator %1 > output\WishListScripted.txt
+
+:: the above only works when using a workspace, not a maven folder
+:: the below works when using a maven folder (-q hides info lines)
+
+mvn test > output/TestResults.txt
+mvn compile exec:java -e -q > output/WishListScripted.txt
--- src/main/java/destiny/WishlistGenerator.java
+++ src/main/java/destiny/WishlistGenerator.java
@@ -38,16 +38,6 @@ public class WishlistGenerator implements AutoCloseable {
 			e.printStackTrace();
 			throw new FileNotFoundException();
 		}
-
-		Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response = Unirest
-                .get("https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/")
-                .header("X-API-KEY", "735ad4372078466a8b68a09ff9c02edb")
-                .asString();
-        System.out.println(response.getBody()); 
-
-
-
 		////
 
 		ArrayList<Object> td = new ArrayList<>();
