146c32f09dc73a8acdd00dac30ee83684f97d864
Daniel Stirling <dannystirl@gmail.com>
2022-06-24 18:20:41 -0400

    changed from http3 to unirest

--- src/main/java/destiny/Item.java
+++ src/main/java/destiny/Item.java
@@ -1,3 +1,5 @@
+package destiny; 
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
--- src/test/java/destiny/AppTest.java
+++ src/test/java/destiny/AppTest.java
@@ -3,17 +3,25 @@ package destiny;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+/** Unit test for simple App. */
+public class AppTest {
+    /** Rigorous Test :-) */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testResponse() throws UnirestException {
+
+        Unirest.setTimeouts(0, 0);
+        HttpResponse<String> response = Unirest
+                .get("https://www.bungie.net/en/OAuth/Authorize?735ad4372078466a8b68a09ff9c02edb&40678")
+                .asString();
+
     }
 }
