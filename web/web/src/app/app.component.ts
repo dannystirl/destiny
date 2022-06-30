5e7b89f7b1a830183cb12565a3ae051abe7e38a2
Daniel Stirling <dannystirl@gmail.com>
2022-06-30 19:40:48 -0400

    saving progress

--- web/web/src/app/app.component.ts
+++ web/web/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Observable, Subject } from '@angular/common/src/facade/async';
+import { Component, Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+
 export class AppComponent {
+  public projectTitle = 'Destiny Applications';
   title = 'Destiny Applications';
+
+  constructor(@Inject(DOCUMENT) private document: Document, @Inject(DataSender) private myService: DataSender) {
+    console.log(this.document.location.href);
+    this.myService.myMethod(this.projectTitle);
+  }
+}
+
+@Injectable()
+export class DataSender {
+  myMethod$: Observable<any>;
+  private myMethodSubject = new Subject<any>();
+
+  constructor() {
+    this.myMethod$ = this.myMethodSubject.asObservable();
+  }
+
+  myMethod(data) {
+    console.log(data);
+    this.myMethodSubject.next(data);
+  }
 }
--- web/web/src/app/app.module.ts
+++ web/web/src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
+
+const ROUTES = [
+  { path: '', component: AppComponent },
+  { path: 'welcome', component: WelcomePageComponent },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forChild(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
