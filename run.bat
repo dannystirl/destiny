04e139e0d0629f56e1e45c3582e4ea60153730d1
Daniel Stirling <77759414+dannystirl@users.noreply.github.com>
2023-03-08 09:49:26 -0500

    updated some wishlist items

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-call java -jar target/wishlistgenerator-1.2.2.jar
+call java -jar target/wishlistgenerator-1.4.1.jar
--- runFresh.bat
+++ runFresh.bat
@@ -7,4 +7,4 @@ call mvn clean compile
 :: dont' actually need this since package calls test
 :: call mvn test -f "e:\Code\wishlistgenerator\pom.xml" -q > output/TestResults.txt
 call mvn package
-call java -jar target/wishlistgenerator-1.2.2.jar
+call java -jar target/wishlistgenerator-1.4.1.jar
